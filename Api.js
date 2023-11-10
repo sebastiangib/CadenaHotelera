c53fbda45a9cac1705c582082b868c4cbc56dc26
David Velez <dvelezalv@cesde.net>
2023-11-10 06:46:05 -0500

    desplegando

--- Api.js
+++ Api.js
@@ -1,5 +1,7 @@
 // Esta clase representa un servidor
 import express from 'express'
+import cors from 'cors'
+
 import { rutasAPI } from './routes/rutasHoteles.js'
 import { establecerConexionBD } from './database/conexion.js'
 
@@ -12,13 +14,14 @@ export class Api{
 
     // 1. Levantar el servidor
     levantarServidor(){
-        this.app.listen(3000,function(){
+        this.app.listen(process.env.PORT,function(){
             console.log("Servidor abierto")
         })
     }
 
     // 2. Atiende las peticiones y responde
     procesarPeticiones(){
+        this.app.use(cors())
         this.app.use(express.json())
         this.app.use("/",rutasAPI)
         
